294eb6a4c8e82606419b6f7f0c83b3d79a9152fb
Jaswanthreddy Nukalapati <143704905+JaswanthReddy2006@users.noreply.github.com>
2026-02-10 23:56:59 +0530

    [347] [Time Beats: 100.00%] [Memory Beats: 22.64%] - LeetPush

--- Top-K-Frequent-Elements.cpp
+++ Top-K-Frequent-Elements.cpp
@@ -1,25 +1,24 @@
 1class Solution {
 2public:
 3    vector<int> topKFrequent(vector<int>& nums, int k) {
-4        // 1. Count frequencies using a hash map
-5        unordered_map<int, int> counts;
+4        unordered_map<int, int> counts;
+5        
 6        for (int num : nums) {
 7            counts[num]++;
 8        }
 9
-10        // 2. Transfer map entries into a vector of pairs {frequency, value}
-11        vector<pair<int, int>> freqVec;
-12        for (auto const& [val, freq] : counts) {
-13            freqVec.push_back({freq, val});
-14        }
-15
-16        sort(freqVec.rbegin(), freqVec.rend());
-17
-18        vector<int> result;
-19        for (int i = 0; i < k; i++) {
-20            result.push_back(freqVec[i].second);
-21        }
-22
-23        return result;
-24    }
-25};
+10        vector<pair<int, int>> freqVec;
+11        for (auto const& [val, freq] : counts) {
+12            freqVec.push_back({freq, val});
+13        }
+14
+15        sort(freqVec.rbegin(), freqVec.rend());
+16
+17        vector<int> result;
+18        for (int i = 0; i < k; i++) {
+19            result.push_back(freqVec[i].second);
+20        }
+21
+22        return result;
+23    }
+24};
